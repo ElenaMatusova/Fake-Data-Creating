540d239043baaedee66a8f1f47bc5c396f73dce8
Elena Matusova <matusova.yelena@gmail.com>
2021-04-18 14:34:25 +0300

    Adding

---
+++ TestData/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new Generate(int.Parse(args[0]), args[1].ToString());
+
+            
+            generator.Start();
+
+            
+        }
+        
+    }
+}
